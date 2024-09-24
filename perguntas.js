40ab20ae4aa7c9127986458c674a1dd0e37da5f6
Anapontes5543 <147190286+Anapontes5543@users.noreply.github.com>
2024-09-24 15:50:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'GEOGRAFIA',
+    'Em que continente fica localizado os EUA',
+    'Fica localizado na América do Norte.'
+)
+
+criaCartao(
+    'MATEMÁTICA',
+    'Qual o valor de pi',
+    'O valor de Pi é infinito. Mas utilizamos o número 3,14.'
+)
+
+criaCartao(
+    'PORTUGUÊS',
+    'Como conjulga o verbo Andar no presente',
+    'EU ando,TU anda, ELE/ELA anda, NÓS andamos, VÓS andais ELES/ELAS andam.'
+)
+
+criaCartao(
+    'INGLÊS',
+    'Como se diz tartaruga em Inglês?',
+    'tartaruga em ingles é turtle.'
+)
+
+criaCartao(
+    'CIÊNCIAS',
+    'Quais os planetas formam o nosso sistema solar?',
+    'Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano, Netuno.'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'Como se chamam os tipos de pinturas que os homens da pré-história desenvolveram nas cavernas?',
+    'São conhecidas como pinturas rupestres.'
+)
